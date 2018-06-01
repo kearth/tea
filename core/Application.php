5385df069b32d6ed9ab0e5835b85f4b8bae833a8
Eric <Eric@Erics-MacBook-Pro.local>
2018-06-01 19:41:13 +0800

    20180601

--- core/Application.php
+++ core/Application.php
@@ -10,6 +10,8 @@ class Application
 
     public static function bootstrap()
     {
+        echo __class__;
+        echo Tea\Core\Base\Stream::class;
         static::seed();
         static::germinate();
         static::grow();
@@ -18,26 +20,8 @@ class Application
 
     private static function seed()
     {
-        try {
-            Mode::detectWhichMode();
-            new Alias();
-        //Alias::set([
-            //'Mode' => '\Tea\Core\Mode'
-            //]);
-        echo 555;
-        Stream::from('Mode')->detectWhichMode()->initTheMode();
-        } catch(Exception $e) {
-            throw new \Exception("为啥");
-            var_export($e->getMessage());
-            exit;
-        }
-
+        Stream::from('Mode')->detectWhichMode()->initTheMode("666");
     }
-
-    //public static function whichMode()
-    //{
-        //return php_sapi_name();
-    //}
 
     private static function germinate()
     {
