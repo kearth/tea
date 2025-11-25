94555186ba8f110aa8aa7e517f8ca912fd648d9b
kearth <kearth@yeah.net>
2025-11-25 19:28:13 +0800

    1.0.0

--- examples/app/api/demo/v1/stream.go
+++ examples/app/api/demo/v1/stream.go
@@ -5,6 +5,10 @@ import (
 	"example/local/app/infra/ecode"
 	"example/local/app/infra/out"
 	"example/local/app/infra/structs"
+	"fmt"
+
+	"github.com/kearth/klib/kerr"
+	"github.com/kearth/tea/frame/base"
 
 	"github.com/gogf/gf/v2/os/gfile"
 	"github.com/kearth/tea/frame/env"
@@ -35,6 +39,10 @@ func (s *Stream) Chat(r *httpserver.Request) {
 	var buffer []byte
 	for scanner.Scan() {
 		buffer = scanner.Bytes()
+		if len(buffer) == 0 {
+			fmt.Println("empty line")
+			break
+		}
 		demo := &structs.Demo{
 			Type: "text",
 			Text: string(buffer),
@@ -43,10 +51,16 @@ func (s *Stream) Chat(r *httpserver.Request) {
 		out.Flush(r)
 	}
 	// 检查扫描错误
+	o := base.Response()
+	var e kerr.Error
 	if err := scanner.Err(); err != nil {
-		out.JSONResponse(r, ecode.ScannerError.Wrap(err), nil)
-		return
+		e = ecode.ScannerError.Wrap(err)
+	} else {
+		e = ecode.Succ
 	}
-	// 响应成功
-	out.JSONResponse(r, ecode.Succ, nil)
+	o.SetCode(e.Code())
+	o.SetMsg(e.Display())
+	o.SetData(nil)
+	out.WriteString(r, o.String())
+	out.Flush(r)
 }
--- examples/app/infra/out/header.go
+++ examples/app/infra/out/header.go
@@ -7,4 +7,5 @@ func StreamHeader(r *ghttp.Request) {
 	r.Response.Header().Set("Content-Type", "text/event-stream")
 	r.Response.Header().Set("Cache-Control", "no-cache")
 	r.Response.Header().Set("Connection", "keep-alive")
+	r.Response.Header().Set("Transfer-Encoding", "chunked")
 }
